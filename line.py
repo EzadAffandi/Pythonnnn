e036e7c9e95c258fc5c5c1ff92ba06edd803ad5c
Nurdini1011 <91740786+Nurdini1011@users.noreply.github.com>
2021-10-08 18:50:05 +0200

    Add files via upload

--- line.py
+++ line.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3 
 import re
+import json
 REMOTE_HOSTNAME="^(\d{1,3})\.\d+\.\d+(\.\d+)$"
 
+#on detecte l'OS utilisé
+def detect_os(user_agent):
+    if 'Windows' in user_agent:
+        return 'Windows'
+    if 'Mac' in user_agent:
+        return 'Mac'
+    if 'Linux' in user_agent:
+        return 'Linux'
+        
 #la fonction qui permet de séparer les infos dans un ligne
 def parse_line(line):
 		ip=line.split(' ')
 		get=line.split('"')
 		return dict(
 			remote_ip=ip[0],
-			time=ip[3]+ip[4],
+			time=ip[3]+" "+ip[4],
 			request=get[1],
 			response=ip[8],
 			byte=ip[9],
 			referrer=ip[10],
-			system_agent=get[5]
+			system_agent=get[5],
+            browser_agent=detect_os(get[5])
 		)
+        
 
-def readF(f):
+def readFichier(f):
     dictparline=[]
 	#ouvrir et parcour par chaque ligne du fichier
     for line in open(f,'r'):
         #enregistrer les infos de chaque ligne dans une liste
         dictparline.append(parse_line(line))
+    
+    with open(f[:-4]+'.json','w') as fg:
+        json.dump(dictparline, fg, indent=8)
     return dictparline
 
 def get_ip(inputt):
@@ -30,8 +45,9 @@ def get_ip(inputt):
 		# if i found a match
 		return match.group(1) 
 	return None
-	
-	
 
-print(readF('apache_logs.log'))
-print(get_ip('46.10.14.53'))
+doc=readFichier('apache_logs.log')
+
+#result=json.dumps(doc , indent = 8)
+#print(result)
+#print(get_ip('46.10.14.53'))
