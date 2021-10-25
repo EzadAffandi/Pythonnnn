57945b6ab7f8e5982b9dbe24fa9de95f66d20199
Nurdini1011 <91740786+Nurdini1011@users.noreply.github.com>
2021-10-25 14:35:37 +0200

    Add files via upload

--- M3206_Python_BIN-ADNAN_BINTI-MOHAMAD_2A/statistique.py
+++ M3206_Python_BIN-ADNAN_BINTI-MOHAMAD_2A/statistique.py
@@ -58,7 +58,7 @@ def statistique_all(f):
     # Affichage du titre
     fenetre.title("Statistique du fichier "+f)
 
-    c = Canvas(fenetre, width=1700, height=800, background="black")
+    c = Canvas(fenetre, width=1700, height=1000, background="black")
     c.pack(expand=True)
     
     #graphe bar pour OS
@@ -147,14 +147,14 @@ def statistique_all(f):
     heure=cherche_heure(dictionaire)
     k=0
     
-    c.create_rectangle(20, 520, 1450, 800, fill="white")
+    c.create_rectangle(20, 520, 1450, 900, fill="white")
     for h in heure: 
       i=heure[h] 
       c.create_rectangle(80+(50*k), 798-i/2, 120+(50*k), 770 , fill="salmon", outline="salmon") 
       c.create_text(80+(50*k)+24, 798-i/2-15,text=i) 
       c.create_text(80+(50*k)+24, 778,text=h+'h') 
       k=k+1    
-    c.create_text(700, 795, text="La statistique des utilisateurs par heure", fill="red")      
+    c.create_text(700, 805, text="La statistique des utilisateurs par heure", fill="red")      
  
     #Démarrage de la boucle Tkinter (à placer à la fin !!!)
     fenetre.mainloop()
@@ -316,9 +316,9 @@ def stat_ip(f):
         else:
             #ligne verticale
             c.create_rectangle(10, 80, 10*len(data_ip), 110, fill="#1E90FF", outline="#1E90FF")
-            c.create_text(10*len(data_ip)+50, 95, text="Total requête ("+str(len(data_ip))+")")
+            c.create_text(10*len(data_ip)+70, 95, text="Total requête ("+str(len(data_ip))+")")
             c.create_rectangle(10, 120, 10*ok, 150, fill="#1020F6", outline="#1020F6")
-            c.create_text(10*ok+40, 135, text="200 OK ("+str(ok)+")")
+            c.create_text(10*ok+50, 135, text="200 OK ("+str(ok)+")")
         
             #afficher les reslutat d'OS utilisé
             y1=160
@@ -326,7 +326,7 @@ def stat_ip(f):
             for i in os_utilise:
                 if os_utilise[i] != 0:
                     c.create_rectangle(10, y1, 10*os_utilise[i], y2, fill="#1E900F", outline="#1E900F")
-                    c.create_text(10*os_utilise[i]+50, y1+15, text=str(i)+" ("+str(os_utilise[i])+")")
+                    c.create_text(10*os_utilise[i]+70, y1+15, text=str(i)+" ("+str(os_utilise[i])+")")
                     y1=y1+50
                     y2=y2+50
               
